cc6637596872669a836203a0f0feda2e0473e8b7
edo979 <prezentabilan@gmail.com>
2022-08-23 11:25:28 +0200

    js basic draft

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const shortcodeApp = (function () {
   const inputEl = document.getElementById('link'),
     btnEl = document.getElementById('fetch-btn'),
-    linkListEl = document.getElementById('link-list')
+    linkListEl = document.getElementById('link-list'),
+    state = []
 
   function setEventListener() {
     btnEl.addEventListener('click', (e) => {
-      console.log('clik on btn')
+      getShortenLink(inputEl.value)
     })
+  }
+
+  async function getShortenLink(link) {
+    console.log('fetching')
+    setState()
+  }
+
+  function setState() {
+    console.log('set state fnc')
+    saveToLS()
+    updateView()
+  }
+
+  function updateView() {
+    console.log('update view fnc')
+  }
+
+  /** Get data from Local storage */
+  function getFromLS() {
+    console.log('checing LS')
+  }
+
+  /** Save data to Local storage */
+  function saveToLS() {
+    console.log('save to LS')
   }
 
   return {
     init: function () {
       setEventListener()
+      getFromLS()
     },
   }
 })()
